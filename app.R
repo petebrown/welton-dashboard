f5db00b0e0b7fe0ec35453289e6ca8ce3a1b8603
Pete Brown <15838790+petebrown@users.noreply.github.com>
2023-05-18 13:57:57 -0400

    updated .gitignore

--- app.R
+++ app.R
@@ -142,12 +142,13 @@ server <- function(input, output, session) {
   output$ssn_records <- DT::renderDataTable(
     get_ssn_records(input$season),
     rownames = FALSE,
-    options = list(
-      pageLength = 5,
-      dom = 'tip',
-      info = FALSE,
-      paging = FALSE
-    )
+    options = list(paging = TRUE,    ## paginate the output
+                   pageLength = 10,  ## number of rows to output for each page
+                   scrollX = TRUE,   ## enable scrolling on X axis
+                   scrollY = TRUE,   ## enable scrolling on Y axis
+                   autoWidth = FALSE, ## use smart column width handling
+                   server = FALSE,   ## use client-side processing
+                   dom = 'frtip')
   )
 
   output$streaks_table <- DT::renderDataTable(
